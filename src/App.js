5fa6b87f2ae383981e44887104e9114aafe05d02
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-05-14 19:00:56 -0400

    error catching

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     this.state = {
       results: null,
       searchKey: '',
-      searchTerm: DEFAULT_QUERY
+      searchTerm: DEFAULT_QUERY,
+      error: null
     };
 
     this.needsToSearchTopStores = this.needsToSearchTopStores.bind(this);
@@ -61,7 +62,7 @@ class App extends Component {
   page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -104,7 +105,8 @@ class App extends Component {
     const { 
       searchTerm, 
       results,
-      searchKey 
+      searchKey,
+      error 
     } = this.state;
 
     const page = (
@@ -130,10 +132,15 @@ class App extends Component {
             Search
           </Search>
         </div>
-          <Table
+        { error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          : <Table
             list={list}
             onDismiss={this.onDismiss}
           />
+        }
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
